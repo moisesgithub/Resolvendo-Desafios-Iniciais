02626c9a71054f98aa07247f956cf8655b1de18e
moisesgithub <moises.github@outlook.com>
2021-10-19 22:33:48 -0300

    Adicionar arquivos de projeto.

---
+++ HO HO HO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HO_HO_HO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = int.TryParse(Console.ReadLine(), out int n);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write("Ho ");
+            }
+            if (n != 0)
+            {
+                Console.WriteLine("Ho!");
+            }            
+        }
+    }
+}
+
+/*
+ using System;
+
+namespace _52._36_URI_1759 {
+    class Program {
+        static void Main(string[] args) {
+           
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++) {
+                if (             ) {
+                    Console.Write("Ho ");                  //Complete o código no espaço em branco
+                }
+                else{
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
+ */
