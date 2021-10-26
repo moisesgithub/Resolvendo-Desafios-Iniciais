2a6f644a1df263b178ba61a0627676cbdfbd7284
moisesgithub <moises.github@outlook.com>
2021-10-25 21:32:03 -0300

    Incluindo mais um desafio!

--- Pneu/Program.cs
+++ Pneu/Program.cs
@@ -1,22 +1,33 @@
-﻿//Escreva um programa que, dada a pressão desejada digitada 
-//pelo motorista e a pressão do pneu lida pela bomba, 
-//indica a diferença entre a pressão desejada e a pressão lida.
-using System;
+﻿//Desenvolva um programa para ler as coordenadas (M,N) 
+//de uma quantidade indeterminada de pontos no sistema cartesiano. 
+//Para cada ponto escrever o quadrante a que ele pertence. 
+//O algoritmo será encerrado quando pelo menos uma de duas 
+//coordenadas for NULA (nesta situação sem escrever mensagem alguma).
 
-class TESTE
+using System; 
+
+class TESTE 
 {
-    static void Main()
-    {
-        int A = int.Parse(Console.ReadLine());
-        int B = int.Parse(Console.ReadLine());
+        public static void Main()
+        {
+            int x, y;
+            while (true)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                x = int.Parse(s[0]);
+                y = int.Parse(s[1]);
 
-        if (A != B)
-        {
-            Console.WriteLine("{0}", A - B);
+                if (x == 0 || y == 0)
+                    break;
+                else if (x > 0 && y > 0)
+                    Console.WriteLine("primeiro");
+                else if (x > 0 && y < 0)
+                    Console.WriteLine("quarto");
+                else if (x < 0 && y < 0)
+                    Console.WriteLine("terceiro");
+                else if ( x < 0 && y > 0)         
+                    Console.WriteLine("segundo");             
+            }
+            Console.ReadLine();
         }
-        else
-        {
-            Console.WriteLine("{0}", B - A);
-        }
-    }
 }
---
+++ Quadrante/Program.cs
@@ -0,0 +1,33 @@
+﻿//Desenvolva um programa para ler as coordenadas (M,N) 
+//de uma quantidade indeterminada de pontos no sistema cartesiano. 
+//Para cada ponto escrever o quadrante a que ele pertence. 
+//O algoritmo será encerrado quando pelo menos uma de duas 
+//coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+
+using System;
+
+class TESTE
+{
+    public static void Main()
+    {
+        int x, y;
+        while (true)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            x = int.Parse(s[0]);
+            y = int.Parse(s[1]);
+
+            if (x == 0 || y == 0)
+                break;
+            else if (x > 0 && y > 0)
+                Console.WriteLine("primeiro");
+            else if (x > 0 && y < 0)
+                Console.WriteLine("quarto");
+            else if (x < 0 && y < 0)
+                Console.WriteLine("terceiro");
+            else if (x < 0 && y > 0)
+                Console.WriteLine("segundo");
+        }
+        Console.ReadLine();
+    }
+}
