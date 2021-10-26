14c1b6c171448d4241c65ed1387374b60246013a
moisesgithub <moises.github@outlook.com>
2021-10-26 20:11:49 -0300

    Guilherme e Suas Pipas

--- GuilhermeSuasPipas/Program.cs
+++ GuilhermeSuasPipas/Program.cs
@@ -7,15 +7,29 @@
 //Guilherme não sabe como determinar a quantidade de barbantes que ele terá que 
 //utilizar para tornar uma pipa de n lados, estável. Você pode ajudá-lo?.
 
-using System; 
- 
-class GuilhermeSuasPipas 
+using System;
+
+class GuilhermeSuasPipas
 {
     static void Main(string[] args)
-    { 
+    {
         long n = long.Parse(Console.ReadLine());
-        long resposta = (n * (n - 3)) / 2; 
-        
+        long resposta = (n * (n - 3)) / 2;
+
         Console.WriteLine(resposta);
     }
 }
+
+/*
+ using System; 
+ 
+class TESTE {
+ 
+    static void Main(string[] args) { 
+        long N = long.Parse(Console.ReadLine());
+        long resposta = (          )) /      ;  //Digite sua lógica aqui
+        Console.WriteLine(resposta);
+    }
+    
+}
+ */
