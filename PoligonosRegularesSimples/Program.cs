1b9b75d0daea417fc2f7259d3454fa6c26d6def3
moisesgithub <moises.github@outlook.com>
2021-11-02 21:15:29 -0300

    Quase quebro a cabeça com esse código, kkk

--- PoligonosRegularesSimples/Program.cs
+++ PoligonosRegularesSimples/Program.cs
@@ -2,29 +2,34 @@
 //o número de lados e o comprimento de cada lado de um polígono 
 //regular (3 ≤ N ≤ 1000000 and 1 ≤ L ≤ 4000).
 
-using System; 
+using System;
+
+namespace PoligonosRegularesSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(int.Parse(v[0]) * int.Parse(v[1]));
+        }
+    }
+}
+
+/*
+ using System; 
 
 class Problem 
 {
     static void Main(string[] args) 
     { 
-        string[] v = Console.ReadLine().Split();
-
+        string[] v = Console.ReadLine().Split(' ');
+        
         int N = int.Parse(v[0]);
         int L = int.Parse(v[1]);
 
         Console.WriteLine(N * L);
-        
-
-
-
-                
-
-
-            
-
-
-
-
     }
 }
+ */
