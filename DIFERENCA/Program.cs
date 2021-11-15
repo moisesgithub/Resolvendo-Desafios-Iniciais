74bbf07fdafc7201c50e7b1ea33b4e08f2b78027
moisesgithub <moises.github@outlook.com>
2021-11-15 11:37:27 -0300

    Desafio DIFERENÇA

--- DIFERENCA/Program.cs
+++ DIFERENCA/Program.cs
@@ -5,20 +5,20 @@ segundo a fórmula: DIFERENCA = (A * B - C * D).
 */
 using System;
 
-namespace Desafio
+namespace DIFERENCA
 {
     class Program
     {
         static void Main(string[] args)
         {
-           int A, B, C, D, DIFERENCA;
-           
+            int A, B, C, D, DIFERENCA;
+
             A = int.Parse(Console.ReadLine());
             B = int.Parse(Console.ReadLine());
             C = int.Parse(Console.ReadLine());
             D = int.Parse(Console.ReadLine());
-            
-            DIFERENCA = (A * B - C * D);
+
+            DIFERENCA = A * B - C * D;
             Console.WriteLine("DIFERENCA = " + DIFERENCA);
         }
     }
---
+++ Intervalo2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Intervalo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tc, a, i;
+
+            int Interval = 0;
+            int outInterval = 0;
+
+            tc = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < tc; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                if (a == 1)
+                {
+                    Interval++;
+                }
+                else
+                {
+                    outInterval++;
+                }
+            }
+            Console.WriteLine("{0} in", Interval);
+            Console.WriteLine("{0} out", outInterval);
+            Console.ReadLine();
+        }
+    }    
+}
+
+/*
+ using System; 
+
+class TESTE {
+
+        public static void Main()
+        {
+           
+            int tc, a, i;
+
+            int Interval = 0;
+            int outInterval = 0;
+
+            tc = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < tc; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                if (                           )
+                {
+                                   ; 
+                }
+                else                           //Insira sua lógica nos espaços em branco
+                {
+                                    ;
+                }
+
+            }
+            Console.WriteLine("{0} in", Interval);
+            Console.WriteLine("{0} out", outInterval);
+
+
+            Console.ReadLine();
+        }
+}
+ */
