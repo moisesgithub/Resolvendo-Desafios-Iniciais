b725dccee11a5cf62995b79da12da21bde5b863d
moisesgithub <moises.github@outlook.com>
2021-12-30 12:06:00 -0300

    Desafio Avanade - Vai Ter Copa?

---
+++ VaiTerCopa/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Desafio
+Kluper é o país sede da copa do mundo. 
+Porém, há muitas pessoas protestando contra o governo. 
+Em redes sociais é possível ver pessoas afirmando que não vai 
+ter copa devido aos protestos e insatisfação política
+
+Mas esses rumores de que não haverá copa são totalmente falsos, 
+e o governo já avisou: vai ter copa sim! 
+
+Entrada
+A entrada contém vários casos de teste. 
+Cada caso de teste consiste de uma linha contendo o número N de reclamações 
+sobre a copa encaminhadas para o governo (0 ≤ N ≤ 100).
+
+Saída
+Para cada teste, a saída consiste de uma linha dizendo "vai ter copa!" 
+caso não haja reclamações para o governo. 
+Caso haja reclamações, a saída deverá dizer "vai ter duas!".
+*/
+using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        string str;
+        while (!string.IsNullOrEmpty(str = Console.ReadLine()))
+        {
+            int n = int.Parse(str);
+            if (n == 0)
+            {
+                Console.WriteLine("vai ter copa!");
+            }
+            else
+            {
+                Console.WriteLine("vai ter duas!");
+            }
+        }
+    }
+}
+/*using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+     string str;
+            while((str=Console.ReadLine())!= null)
+            {
+                if (   )
+                {
+                       
+                }
+                else          //Escreva o seu código nos espaços em branco
+                { 
+                    int x = int.Parse(str);
+                    if (         )
+                    {
+                    Console.WriteLine(   ); 
+                    }
+                    else
+                    {
+                        Console.WriteLine(  );
+                    }
+                }
+            }
+
+    }
+
+}
+*/
