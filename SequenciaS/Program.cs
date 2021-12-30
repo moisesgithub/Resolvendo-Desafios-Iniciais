35d8fcc5cdec245a688ecd3eb9af89f2f8d2672e
moisesgithub <moises.github@outlook.com>
2021-12-30 11:53:50 -0300

    Desafio Avanade - Sequência_IJ2

---
+++ SequenciaS/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Escreva um algoritmo para calcular e escrever o valor de S, sendo S dado pela fórmula:
+S = 1 + 1/2 + 1/3 + … + 1/100
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+A saída contém um valor correspondente ao valor de S.
+O valor deve ser impresso com dois dígitos após o ponto decimal.
+*/
+using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        double s = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            s += 1.0 / i;
+        }
+        Console.WriteLine("{0:0.00}", s);
+    }
+}
+/*
+using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+      double a, c, S= 0;
+            for (      )
+            {
+                c =           ;  //coloque a sua lógica nos espaços em branco
+                S +=        ;
+            }
+            var x=Math.Round(S,2);
+            Console.WriteLine(      );
+
+    }
+
+}
+*/
---
+++ Sequencia_IJ2/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo:
+I=1 J=7
+I=1 J=6
+I=1 J=5
+I=3 J=7
+I=3 J=6
+I=3 J=5
+I=5 J=7
+I=5 J=6
+I=5 J=5
+I=7 J=7
+I=7 J=6
+I=7 J=5
+I=9 J=7
+I=9 J=6
+I=9 J=5
+*/
+using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        foreach (int i in new int[] { 1, 3, 5, 7, 9 })
+        {
+            foreach (int j in new int[] { 7, 6, 5 })
+            {
+                Console.WriteLine("I={0} J={1}", i, j);
+            }
+        }
+    }
+}
+/*
+using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+       for(           )
+            {
+                for(        )    //escreva seu código nos espaços em branco
+                {
+                    Console.WriteLine(       );
+                }
+            }
+    }
+
+}
+*/
