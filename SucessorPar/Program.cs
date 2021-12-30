565e0045bad7170521ccc54ebf253761564e0e14
moisesgithub <moises.github@outlook.com>
2021-12-30 12:22:29 -0300

    Desafio Avanade - Sucessor Par

---
+++ SucessorPar/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Para se preparar para os outros problemas, vamos fazer um teste. 
+Dado um número X, retorne o menor número par maior do que X.
+
+Entrada
+Uma linha contendo um número  0 < X < 107.
+
+Saída
+Uma linha contendo a resposta do problema.
+*/
+using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y;
+        if (x % 2 == 0)
+        {
+            y = x + 2;
+        }
+        else
+        {
+            y = x + 1;
+        }
+        Console.WriteLine(y);
+    }
+}
+/*
+using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+    int x = int.Parse(Console.ReadLine());
+            
+            //escreva aqui o seu código
+
+    }
+
+}
+*/
